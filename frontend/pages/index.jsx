b0abdbdaa918618ac9ec92772e7394160f8388bd
VagnerAndrei26 <vagnerandrei11@gmail.com>
2023-02-10 19:38:02 +0200

    third commit

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -8,7 +8,7 @@ import {
   CRYPTODEVS_DAO_CONTRACT_ADDRESS,
   CRYPTODEVS_NFT_ABI,
   CRYPTODEVS_NFT_CONTRACT_ADDRESS,
-} from "../constants";
+} from "../constants.js";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
